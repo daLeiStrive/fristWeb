ecd3e01805e9f52fc6b4c41ae0c2d1a7c8225f77
dalei <1782608440@qq.com>
2017-06-13 17:55:35 +0800

    一个简单的请求

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,9 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -24,10 +28,31 @@ public class HomeController {
 
     //映射一个action
     @RequestMapping("one.do")
-    public  String index(HttpServletRequest request,HttpServletResponse response){
+    public  String index(HttpServletRequest reques){
+//        ,HttpServletResponse response
         //输出日志文件
         logger.info("the first jsp pages");
         //返回一个index.jsp这个视图
         return "index";
     }
+    @ResponseBody
+    @RequestMapping("two.do")
+    public Map testLogin2(HttpServletRequest request){
+        // request和response不必非要出现在方法中，如果用不上的话可以去掉
+        // 参数的名称是与页面控件的name相匹配，参数类型会自动被转换
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Map<String,Object> testMap = new HashMap<>();
+        testMap.put("name","小唐");
+        testMap.put("age","小wang");
+
+        /*Map resultMap = new HashMap();
+        ModelAndView mav = new ModelAndView("/bids/bids");
+        resultMap.put("modelAndView", mav);*/
+        return testMap;  // 采用重定向方式跳转页面
+        // 重定向还有一种简单写法
+        // return new ModelAndView("redirect:../index.jsp");
+    }
+
+
 }
