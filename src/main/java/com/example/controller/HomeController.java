a2b997a521342171e97f081de2e95960d4b22a80
dalei <1782608440@qq.com>
2017-06-14 15:22:49 +0800

    一个简单的请求

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -3,15 +3,22 @@ package com.example.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -35,7 +42,7 @@ public class HomeController {
         //返回一个index.jsp这个视图
         return "index";
     }
-    @ResponseBody
+    @ResponseBody//返回json
     @RequestMapping("two.do")
     public Map testLogin2(HttpServletRequest request){
         // request和response不必非要出现在方法中，如果用不上的话可以去掉
@@ -53,6 +60,45 @@ public class HomeController {
         // 重定向还有一种简单写法
         // return new ModelAndView("redirect:../index.jsp");
     }
+    @ResponseBody//返回json
+    @RequestMapping("fileUpload.do")
+    public Map fileUpload(HttpServletRequest request) {
+//创建一个通用的多部分解析器.
+        CommonsMultipartResolver commonsMultipartResolver = new
+                CommonsMultipartResolver(request.getSession().getServletContext());
+        request.getParameter("file_data");
+        //设置编码
+        commonsMultipartResolver.setDefaultEncoding("utf-8");
+        //判断 request 是否有文件上传,即多部分请求...
+        if (commonsMultipartResolver.isMultipart(request))
+        {
+            //转换成多部分request
+            MultipartHttpServletRequest multipartRequest =(MultipartHttpServletRequest) request;
+            // 获得上传的文件（根据前台的name名称得到上传的文件）
+            MultiValueMap<String, MultipartFile> multiValueMap = multipartRequest.getMultiFileMap();
+            // file 是指 文件上传标签的 name=值
+            List<MultipartFile> file = multiValueMap.get("exampleInputFile");
+            // 判断文件是否为空
+            if (!file.isEmpty()) {
+                try {
+                    for(int i = 0; i < file.size(); i++){
+                        MultipartFile f = file.get(i);
+                        // 文件保存路径
+                        String filePath = "D:\\myFristWeb\\src\\main\\webapp\\fileUpload\\temp\\"
+                                + f.getOriginalFilename();
+                        f.transferTo(new File(filePath));
+                    }
+                    // 转存文件
+//                    file.transferTo(imageFile);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
+        }
+
+        // 重定向
+        return new HashMap<>();
+    }
 
 }
--- src/main/webapp/bizmodules/views/jsp/index.jsp
+++ src/main/webapp/bizmodules/views/jsp/index.jsp
@@ -12,32 +12,36 @@
 
 </head>
 <body style="padding-left: 10%;padding-right: 10%; padding-top: 20px;background-color: whitesmoke">
-<form>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputFile">File input</label>
-        <input type="file" id="exampleInputFile" class="file">
-        <p class="help-block">Example block-level help text here.</p>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="checkbox"> Check me out
-        </label>
-    </div>
-    <button type="button" class="btn btn-default" id="submit">Submit</button>
-</form>
-<%@include file="biz_meta.jsp" %>
-<script src="/bizmodules/statics/modules/index/IndexContoller.js"></script>
+<div class="container" style="    padding: 40px;background-color: white;">
+    <form>
+        <div class="form-group col-md-6">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group col-md-12" style="    height: 300px;margin-bottom: 15%;">
+            <label >File input</label>
+            <input type="file" id="exampleInputFile" name="exampleInputFile" class="file"
+                   multiple data-show-upload ="true" data-show-caption ="true">
+        </div>
+        <div class="form-group col-md-12 text-center ">
+        <%--<div class="checkbox">
+            <label>
+                <input type="checkbox"> Check me out
+            </label>
+        </div>--%>
+        <button type="button" class="btn btn-primary col-md-2 clearfix" id="submit">Submit</button>
+        </div>
 
+    </form>
+</div>
+<%@include file="biz_meta.jsp" %>
+<script src="/bizmodules/modules/index/IndexContoller.js"></script>
 <script src="/bizmodules/statics/js/fileinput4.4/fileinput.min.js"></script>
-<script src="<%=jspParamsJs%>js/fileinput4.4/fileinput_locale_zh.min.js"></script>
+<script src="/bizmodules/statics/js/fileinput4.4/fileinput_locale_zh.min.js"></script>
 </body>
 
 </html>
